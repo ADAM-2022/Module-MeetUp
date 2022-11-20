29f97f33d73ddfa0aa1bf21aecf59ce24cd552b9
Taekhwan Han <hth3396@gmail.com>
2022-11-20 15:00:26 +0900

    feat: #33 PostechMenuCalendar 날짜 불러오는 함수 구현 현재 날짜부터 일주일간의 날짜를 불러오는 함수 구현

--- Module-MeetUP/Module-MeetUP/Screens/PostechMenuView/ViewComponents/MenuWeeklyCalendarView.swift
+++ Module-MeetUP/Module-MeetUP/Screens/PostechMenuView/ViewComponents/MenuWeeklyCalendarView.swift
@@ -8,6 +8,15 @@
 import SwiftUI
 
 struct MenuWeeklyCalendarView: View {
+    //TODO: 후에 PostechMenuStateHolder.swift로 옮길 로직
+    func getDateRange() -> [Date] {
+        var dateRange: [Date] = []
+        for addDate in 0..<7 {
+            dateRange.append(Calendar.current.date(byAdding: .day, value: addDate, to: Date()) ?? Date())
+        }
+        return dateRange
+    }
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
