c298c9a02d45bc2aed78d3ee209ca1121e91b8b9
Taekhwan Han <hth3396@gmail.com>
2022-12-20 23:59:42 +0900

    feat: #33 시간대 타이틀 tuple로 변경 아침,점심,저녁 및 시간대에 대한 타이틀을 tuple 배열로 변경

--- Module-MeetUP/Module-MeetUP/Screens/PostechMenuView/ViewComponents/RestaurantsViewComponents/MenuTimeSlotView.swift
+++ Module-MeetUP/Module-MeetUP/Screens/PostechMenuView/ViewComponents/RestaurantsViewComponents/MenuTimeSlotView.swift
@@ -8,14 +8,16 @@
 import SwiftUI
 
 struct MenuTimeSlotView: View {
+    private let timeSlot: [(String, String)] = [("아침", "07:30 - 09:30"), ("점심", "11:30 - 13:30"), ("저녁", "17:30 - 19:00")]
+    
     var body: some View {
         HStack(alignment: .firstTextBaseline, spacing: .zero) {
-            Text("점심")
+            Text(timeSlot[0].0)
                 .font(.system(size: 24))
                 .fontWeight(.bold)
                 .foregroundColor(.black)
                 .padding(.trailing, 6)
-            Text("11:50 - 13:00")
+            Text(timeSlot[0].1)
                 .font(.system(size: 14))
                 .foregroundColor(.blue)
             Spacer()
